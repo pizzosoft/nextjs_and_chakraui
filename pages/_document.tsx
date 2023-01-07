07bb143b1d56bdc8b52ac6206f09a9c3f2c1bcfc
Raphael Pizzo <raphael.pizzo@pizzosoft.com.br>
2023-01-07 15:37:14 -0300

    Limpeza do projeto
    Limpeza do projeto

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,11 @@
+import { ColorModeScript } from '@chakra-ui/react'
 import { Html, Head, Main, NextScript } from 'next/document'
-
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang='en'>
       <Head />
       <body>
+        <ColorModeScript />
         <Main />
         <NextScript />
       </body>
